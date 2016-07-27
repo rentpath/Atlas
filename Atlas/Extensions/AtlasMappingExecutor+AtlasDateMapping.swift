a3fabe121267138b913d751d9625bf3c754b7c4c
Jeremy Fox <atljeremy@me.com>
2016-07-27 06:58:55 -0400

    Refactoring Atlas to make it more modular and extensible

--- Atlas/Extensions/AtlasMappingExecutor+AtlasDateMapping.swift
+++ Atlas/Extensions/AtlasMappingExecutor+AtlasDateMapping.swift
@@ -1,24 +1,95 @@
-//
-//  AtlasDateMapper.swift
-//  Atlas
-//
-//  Created by Jeremy Fox on 7/25/16.
-//  Copyright © 2016 RentPath. All rights reserved.
-//
+/*
+ * Copyright (c) 2016 RentPath, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 extension AtlasMappingExecutor: AtlasDateMapping {
     
-    func mapKey(key: String) throws -> Self {
-        keyIsOptional = false
-        self.key = key
-        return self
+    func dateFromRFC3339StringFromKey(key: String, withinJSONObject object: [String: JSON]?) throws -> NSDate? {
+//        guard let key = key else {
+//            throw MappingError.NoKeyError
+//        }
+        
+        guard let _val = object?[key] as? String where !_val.isEmpty else {
+            throw MappingError.NotMappable("The value of key \(key) in the provided JSON object isn't a String and therefore cannot be mapped to an NSDate.")
+        }
+        
+        guard let date = NSDate.dateFromString(_val, withFormat: .RFC3339) else {
+            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the RFC3339 format.")
+        }
+        
+        return date
     }
     
-    func mapOptionalKey(key: String) throws -> Self {
-        keyIsOptional = true
-        self.key = key
-        return self
+    func dateFromRFC3339StringFromOptionalKey(key: String, withinJSONObject object: [String: JSON]?) throws -> NSDate? {
+//        guard let key = key else {
+//            throw MappingError.NoKeyError
+//        }
+        
+        guard let _val = object?[key] as? String where !_val.isEmpty else {
+            return nil
+        }
+        
+        guard let date = NSDate.dateFromString(_val, withFormat: .RFC3339) else {
+            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the RFC3339 format.")
+        }
+        
+        return date
     }
+    
+    func dateFromKey(key: String, toDateWithFormat format: String, withinJSONObject object: [String: JSON]?) throws -> NSDate? {
+//        guard let key = key else {
+//            throw MappingError.NoKeyError
+//        }
+        
+        guard let _val = object?[key] as? String where !_val.isEmpty else {
+            throw MappingError.NotMappable("The value of key \(key) in the provided JSON object isn't a String and therefore cannot be mapped to an NSDate.")
+        }
+        
+        guard let date = NSDate.dateFromString(_val, withFormat: .Custom(format)) else {
+            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the RFC3339 format.")
+        }
+        
+        return date
+    }
+    
+    func dateFromOptionalKey(key: String, toDateWithFormat format: String, withinJSONObject object: [String: JSON]?) throws -> NSDate? {
+//        guard let key = key else {
+//            throw MappingError.NoKeyError
+//        }
+        
+        guard let _val = object?[key] as? String where !_val.isEmpty else {
+            return nil
+        }
+        
+        guard let date = NSDate.dateFromString(_val, withFormat: .Custom(format)) else {
+            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the RFC3339 format.")
+        }
+        
+        return date
+    }
+    
+    
+    
+    
+    
     
     /**
      
@@ -29,9 +100,9 @@ extension AtlasMappingExecutor: AtlasDateMapping {
      - Returns: An Optional NSDate
      
      */
-    func toRFC3339Date() throws -> NSDate? {
-        return try toDateWithFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'")
-    }
+//    func toRFC3339Date() throws -> NSDate? {
+//        return try toDateWithFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'")
+//    }
     
     /**
      
@@ -44,30 +115,30 @@ extension AtlasMappingExecutor: AtlasDateMapping {
      - Returns: An Optional NSDate
      
      */
-    func toDateWithFormat(format: String) throws -> NSDate? {
-        guard let key = key else {
-            if keyIsOptional {
-                return nil
-            } else {
-                throw MappingError.NoKeyError
-            }
-        }
-        
-        guard let _val = jsonObject?[key] as? String where !_val.isEmpty else {
-            
-            if keyIsOptional {
-                return nil
-            } else {
-                throw MappingError.NotMappable("The value of key \(key) in the provided JSON object isn't a String and therefore cannot be mapped to an NSDate.")
-            }
-        }
-        
-        guard let date = NSDate.dateFromString(_val, withFormat: .Custom(format)) else {
-            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the format \(format)")
-        }
-        
-        return date
-    }
-
+//    func toDateWithFormat(format: String) throws -> NSDate? {
+//        guard let key = key else {
+//            if keyIsOptional {
+//                return nil
+//            } else {
+//                throw MappingError.NoKeyError
+//            }
+//        }
+//        
+//        guard let _val = jsonObject?[key] as? String where !_val.isEmpty else {
+//            
+//            if keyIsOptional {
+//                return nil
+//            } else {
+//                throw MappingError.NotMappable("The value of key \(key) in the provided JSON object isn't a String and therefore cannot be mapped to an NSDate.")
+//            }
+//        }
+//        
+//        guard let date = NSDate.dateFromString(_val, withFormat: .Custom(format)) else {
+//            throw MappingError.NotMappable("The date string \(_val) of key \(key) in the provided JSON object does not match the format \(format)")
+//        }
+//        
+//        return date
+//    }
+//
     
 }
--- Atlas/Protocols/AtlasDateMapping.swift
+++ Atlas/Protocols/AtlasDateMapping.swift
@@ -1,14 +1,78 @@
-//
-//  AtlasDateMapper.swift
-//  Atlas
-//
-//  Created by Jeremy Fox on 7/25/16.
-//  Copyright © 2016 RentPath. All rights reserved.
-//
+/*
+ * Copyright (c) 2016 RentPath, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 public protocol AtlasDateMapping {
-    func mapKey(key: String) throws -> Self
-    func mapOptionalKey(key: String) throws -> Self
-    func toRFC3339Date() throws -> NSDate?
-    func toDateWithFormat(format: String) throws -> NSDate?
+    
+    /**
+     Map an RFC3339 date string to an instance of NSDate
+     
+     - Parameters:
+        - key: The String to use to pull the value from `object`
+        - object: The Dictonary<String, JSON> used with `key` to pull the value from
+     
+     - Throws: Will throw a `MappingError` if the `key` doesn't exist within `object`.  Also will throw a `MappingError` if the value of `key` in `object` is not a string type
+     
+     - Returns: An Optional NSDate
+     */
+    func dateFromRFC3339StringFromKey(key: String, withinJSONObject object: [String: JSON]?) throws -> NSDate?
+    
+    /**
+     Optionally map an RFC3339 date string to an instance of NSDate
+     
+     - Parameters:
+        - key: The String to use to pull the value from `object`
+        - object: The Dictonary<String, JSON> used with `key` to pull the value from
+     
+     - Throws: A `MappingError` will be thrown if the value of `key` in `object` is not a string type
+     
+     - Returns: An Optional NSDate
+     */
+    func dateFromRFC3339StringFromOptionalKey(key: String, withinJSONObject object: [String: JSON]?) throws -> NSDate?
+    
+    /**
+     Map a date string to an instance of NSDate
+     
+     - Parameters:
+        - key: The String to use to pull the value from `object`
+        - format: String repesenting the date format to use when parsing the date string
+        - object: The Dictonary<String, JSON> used with `key` to pull the value from
+     
+     - Throws: Will throw a `MappingError` if the `key` doesn't exist within `object`.  Also will throw a `MappingError` if the value of `key` in `object` is not a string type
+     
+     - Returns: An Optional NSDate
+     */
+    func dateFromKey(key: String, toDateWithFormat format: String, withinJSONObject object: [String: JSON]?) throws -> NSDate?
+    
+    /**
+     Optionally map a date string to an instance of NSDate
+     
+     - Parameters:
+        - key: The String to use to pull the value from `object`
+        - format: String repesenting the date format to use when parsing the date string
+        - object: The Dictonary<String, JSON> used with `key` to pull the value from
+     
+     - Throws: A `MappingError` will be thrown if the value of `key` in `object` is not a string type
+     
+     - Returns: An Optional NSDate
+     */
+    func dateFromOptionalKey(key: String, toDateWithFormat format: String, withinJSONObject object: [String: JSON]?) throws -> NSDate?
 }
