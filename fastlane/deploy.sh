20bca5436c72deed2aaa2e8479dea40600943f3b
Ali Illyas <ali@illyas.net>
2017-02-08 17:52:56 -0600

    [#139428587] Convert Frameworks to use Bitrise

--- fastlane/deploy.sh
+++ fastlane/deploy.sh
@@ -4,5 +4,5 @@ git config --global user.name "build user"
 export OLD_GIT_TAG=$(git tag --sort version:refname | tail -n1)
 export NEW_GIT_TAG=$(echo $OLD_GIT_TAG | (IFS=".$IFS" ; read a b c && echo $a.$b.$((c + 1))))
 git tag $NEW_GIT_TAG -a -m "Generated tag $NEW_GIT_TAG"
-if [ "$TRAVIS_BRANCH" == "master" ]; then git push --quiet https://$GITHUB_ACCESS_TOKEN@github.com/$ORGANIZATION/$REPOSITORY $NEW_GIT_TAG > /dev/null 2>&1; fi
-if [ "$TRAVIS_BRANCH" == "master" ]; then ../release_notes.sh $OLD_GIT_TAG $NEW_GIT_TAG; fi
+if [ "$BITRISE_GIT_BRANCH" == "master" ]; then git push --quiet https://$GITHUB_ACCESS_TOKEN@github.com/$ORGANIZATION/$REPOSITORY $NEW_GIT_TAG > /dev/null 2>&1; fi
+if [ "$BITRISE_GIT_BRANCH" == "master" ]; then ../release_notes.sh $OLD_GIT_TAG $NEW_GIT_TAG; fi
