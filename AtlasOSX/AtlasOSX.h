ebbb5d4ae15a163652d0dbc2fcef87f7d4f9bfd9
Jeremy Fox <atljeremy@me.com>
2017-09-26 21:00:25 -0400

    Added OS X target

---
+++ AtlasOSX/AtlasOSX.h
@@ -0,0 +1,19 @@
+//
+//  AtlasOSX.h
+//  AtlasOSX
+//
+//  Created by Jeremy Fox on 9/26/17.
+//  Copyright © 2017 RentPath. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AtlasOSX.
+FOUNDATION_EXPORT double AtlasOSXVersionNumber;
+
+//! Project version string for AtlasOSX.
+FOUNDATION_EXPORT const unsigned char AtlasOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtlasOSX/PublicHeader.h>
+
+
